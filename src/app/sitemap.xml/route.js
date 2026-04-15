b5effd214d2be8cf6225aa020169ed77cd21c198
areebhussainshiwantek <areeb.hussain@shiwantek.com>
2026-04-15 08:45:21 -0700

    Update route.js

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,5 +1,5 @@
 export async function GET() {
-  const res = await fetch('https://backend.sparemicro.com/sitemap/xmlsitemap')
+  const res = await fetch('https://backend.sparemicro.com/api/sitemap/xmlsitemap')
   const xml = await res.text()
 
   return new Response(xml, {
