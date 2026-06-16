fdca14d05f04f2efb9b6803a23fe407135ed1698
Areeb Hussain <areeb.hussain@shiwantek.com>
2026-06-16 12:50:38 -0700

    pull from main quantity typing not working on product page | Shipping not working on zipping code. | Google pay not integrated

--- src/app/components/Product/ProductMiddle.tsx
+++ src/app/components/Product/ProductMiddle.tsx
@@ -20,6 +20,8 @@ const ProductMiddle = ({ product, quantity, increment, decrement, setQuantity }:
   const router = useRouter();
   const minQty = product?.minPurchaseQuantity || 1;
   const maxQty = product?.maxPurchaseQuantity;
+  const robotoCondensed = "'Roboto Condensed'";
+  const roboto = "'Roboto ', Arial, Helvetica, sans-serif";
   const cart = useAppSelector((state: RootState) => state.cart.items);
   const { reviews, reviewsLoading, reviewsError, stats } = useAppSelector(
     (state) => state.home
@@ -44,7 +46,7 @@ const ProductMiddle = ({ product, quantity, increment, decrement, setQuantity }:
       <section className="product-middle flex flex-col h-full w-full max-w-full  xl:max-w-[50%] 2xl:max-w-[50%] ">
         {/* Title Section */}
         <div className="flex flex-col gap-2 mb-4">
-          <h1 className="font-bold text-[18px] sm:text-[18px] md:text-[18px] lg:text-[20px] xl:text-[20px] 2xl:text-[20px] leading-tight text-[#545454] border-b-1 border-[#8b8b8b] pb-3">
+          <h1 className="font-bold text-[18px] sm:text-[18px] md:text-[18px] lg:text-[20px] xl:text-[20px] 2xl:text-[20px] leading-tight text-[#545454] border-b-1 border-[#8b8b8b] pb-3"  style={{fontFamily:robotoCondensed}}>
             {product?.name || "N/A"}
           </h1>
 
