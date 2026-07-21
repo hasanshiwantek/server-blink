d6f30d871f08513b3732c1c3bafa02afb16d5cb6
raoali-tech <ali.murad@shiwantek.com>
2026-07-21 12:27:32 -0700

    pull from mkain

--- src/app/components/CheckoutComponent/CheckoutComponent.tsx
+++ src/app/components/CheckoutComponent/CheckoutComponent.tsx
@@ -1371,7 +1371,7 @@ const CheckoutForm = () => {
 
     saveTimeoutRef.current = setTimeout(() => {
       const shippingFormData = {
-        email: watchedValues.email || "",
+        email: auth?.user?.email || watchedValues.email || "",
         firstName: watchedValues.firstName || "",
         lastName: watchedValues.lastName || "",
         company: watchedValues.company || "",
